4a8fe09a04c418f3a75bb0b285fdaf8c3cf2126a
서동억 <@192.168.1.40>
2017-08-17 15:31:35 +0900

    3번째입니다.

--- JavaGit/src/ch01/Welcome.java
+++ JavaGit/src/ch01/Welcome.java
@@ -4,5 +4,6 @@ public class Welcome {
 	public static void main(String args[]) {
 		System.out.println("안녕하세요 반가워요");
 		System.out.println("네 반가워요 착하게 생기셨네요!");
+		System.out.println("네 감사합니다. 3번째입니다.");
 	}
 }
